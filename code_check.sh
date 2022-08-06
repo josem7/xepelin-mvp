cb7d2b7e4177e2d11dc65feafad2de567f27163d
Jose Dominguez <jndominguez@uc.cl>
2022-08-06 12:38:26 -0400

    fix lint

--- code_check.sh
+++ code_check.sh
@@ -1,3 +1,3 @@
-docker-compose run api poetry run black . \
-    && docker-compose run api poetry run prospector \
-    && docker-compose run api python3 manage.py test -v 2
+docker-compose run mvp poetry run black . \
+    && docker-compose run mvp poetry run prospector \
+    && docker-compose run mvp python3 manage.py test -v 2
