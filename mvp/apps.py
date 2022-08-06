c1b5784084b73bd6171f74d5bf020976f71108a1
Jose Dominguez <jndominguez@uc.cl>
2022-08-06 12:57:54 -0400

    fix black

--- mvp/apps.py
+++ mvp/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class MvpConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mvp'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "mvp"
--- xepelin/wsgi.py
+++ xepelin/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'xepelin.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "xepelin.settings")
 
 application = get_wsgi_application()
